3d4be511438a629d74d1da94bf80c442fb3b961b
Cristina Munoz <mcmguaba@gmail.com>
2014-05-04 16:12:46 -0700

    Chapter 4 solutions.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,47 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+@KBD
+D=M
+@23
+D;JNE // if key pressed, go below and fill screen
+@6
+D;JEQ // else, clear the screen
+
+@i
+M=0
+@i
+D=M
+@8192
+D=A-D 
+@0
+D;JEQ
+@i
+D=M
+@SCREEN
+A=D+A
+M=0 //fill screen
+@i 
+M=M+1
+@8
+0;JMP
+
+@i
+M=0
+@i
+D=M
+@8192
+D=A-D 
+@0
+D;JEQ
+@i
+D=M
+@SCREEN
+A=D+A
+M=-1 //fill screen
+@i 
+M=M+1
+@25
+0;JMP
+
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -4,6 +4,29 @@
 // File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    @0 
+    D=M
+    @i 
+    M=D //initialize counter: i=R0
+    @2 
+    M=0 //initialize result: R2=0
+(LOOP)
+    @i
+    D=M
+    @END
+    D;JEQ //if i=0, return
+    @1
+    D=M
+    @2 
+    M=M+D //R2 = R2 + R1
+    @i
+    M=M-1 //Decrement i
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
